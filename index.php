48faaff20a9083896a2fc970d416357b4985a4dd
A1n2F <alissonfferreira22@gmail.com>
2025-04-14 16:01:17 -0300

    Configurando o projeto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Wise</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <div class="text-red-500">Hello World</div>
+</body>
+</html>
