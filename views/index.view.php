216057298f913b707f744c00f05467abd0f6286f
A1n2F <alissonfferreira22@gmail.com>
2025-04-16 09:35:02 -0300

    Criando a classe DB

--- views/index.view.php
+++ views/index.view.php
@@ -16,7 +16,7 @@
 <div class="p-2 border-stone-800 border-2 bg-stone-900 rounded">
     <div class="flex">
         <div class="w-1/3">
-            <image src=<?=$livro['img']?> alt="" class="w-40 h-50" />
+            <image src="img/<?=$livro['img']?>" alt="" class="w-40 h-50" />
         </div>
         <div class="space-y-1 ml-2">
             <a href="/livro?id=<?=$livro['id']?>" class="font-semibold hover:underline"><?=$livro['title']?></a>
