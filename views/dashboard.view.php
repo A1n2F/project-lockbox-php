98108771f34a453abdbfadc6d67f8c0e18b3575c
A1n2F <alissonfferreira22@gmail.com>
2025-04-23 15:03:10 -0300

    Criando o layout da barra de pesquisa

--- views/dashboard.view.php
+++ views/dashboard.view.php
@@ -17,4 +17,30 @@
         </ul>
     </div>
     </div>
+
+<div class="flex space-x-4 w-full">
+    <form class="w-full">
+        <label class="input w-full">
+
+        <input type="text" name="pesquisar" class="grow" placeholder="Pesquisar notas..." />
+        
+        <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <g
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2.5"
+            fill="none"
+            stroke="currentColor"
+            >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+            </g>
+        </svg>
+
+        </label>
+    </form>
+
+    <a href="#" class="btn btn-primary">+ Item</a>
+</div>
+
 </div>
