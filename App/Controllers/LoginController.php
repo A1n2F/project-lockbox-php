7d2e8f275aafae05d471b31e538826b521fa719c
A1n2F <alissonfferreira22@gmail.com>
2025-04-23 09:56:59 -0300

    Refatorando sistema de rotas

---
+++ App/Controllers/LoginController.php
@@ -0,0 +1,10 @@
+<?php
+    namespace App\Controllers;
+
+    class LoginController {
+        public function index() {}
+
+        public function login() {}
+    }
+
+?>
--- routes.php
+++ routes.php
@@ -1,4 +1,15 @@
-<?php 
+<?php
+
+use Core\Route;
+
+use App\Controllers\IndexController;
+use App\Controllers\LoginController;
+
+    (new Route($routes))
+    ->get('/', IndexController::class)
+    ->get('/login', [ LoginController::class, 'index' ])
+    ->post('/login', [ LoginController::class, 'login' ])
+    ->run();
 
     $controller = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
 
